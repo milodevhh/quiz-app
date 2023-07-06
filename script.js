5fb085abc881d944cd354a5972f443a188b84ea1
milodevhh <schmiesseklgmail.com>
2023-07-06 11:36:17 +0200

    add eventListener bookmark

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 console.log("hey");
 
-const wineBottle = document.querySelector('[data-js="wineBottleBookmark"]');
+const wineBottleButton = document.querySelector(
+  '[data-js="wineBottleBookmark"]'
+);
+const wineBottleSVG = document.querySelector(
+  '[data-js="svgWineBottleBookmarkClicked"]'
+);
+
+console.log(wineBottleButton);
+console.log(wineBottleSVG);
+
+wineBottleButton.addEventListener("click", (event) => {
+  wineBottleSVG.classList.toggle("wine-bottle--clicked");
+  console.log("bitte");
+});
