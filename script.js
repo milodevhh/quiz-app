4b6691693306c7da9f488046651a5ade96343627
milodevhh <schmiesseklgmail.com>
2023-07-06 11:57:44 +0200

    add toggle button by card

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ wineBottleButton.addEventListener("click", (event) => {
   wineBottleSVG.classList.toggle("wine-bottle--clicked");
   console.log("bitte");
 });
+
+function toggleAnswer() {
+  var answerBtn = document.getElementById("answerButton");
+  var answerText = document.getElementById("answerText");
+
+  if (
+    answerText.classList.contains("question-card__answer-container--hidden")
+  ) {
+    answerText.classList.remove("question-card__answer-container--hidden");
+    answerBtn.textContent = "Hide Answer";
+  } else {
+    answerText.classList.add("question-card__answer-container--hidden");
+    answerBtn.textContent = "Show Answer";
+  }
+}
